fa7753d8d5bda739f105df2d298419694c588058
jres <ljresetl@gmail.com>
2025-09-30 12:59:07 +0200

    proba5

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from './components/App/App';
 import 'modern-normalize/modern-normalize.css'
 import './index.css'
 
